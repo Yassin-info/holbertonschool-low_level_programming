fbbc67fefddf1ba7952c8c026ed453127114a17d
root <yassin.jaghmim@gmail.com>
2024-10-24 11:24:37 +0000

    4-print_alphabt.c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,15 +1,17 @@
-#include <unistd.h>
-
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 char letter;
-
 for (letter = 'a'; letter <= 'z'; letter++)
 {
 if (letter != 'e' && letter != 'q')
 putchar(letter);
 }
 putchar('\n');
-
 return (0);
 }
