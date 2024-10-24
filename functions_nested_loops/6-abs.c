85562723ffe7adc037bb11ccbc4b3174b82b9f5d
root <yassin.jaghmim@gmail.com>
2024-10-24 17:41:19 +0000

    7-print_last_digit.c

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,20 +1,14 @@
 #include "main.h"
-#include <stdio.h>
 /**
-* main - check the code
+* _abs - Computes the absolute value of an integer
+* @n: The integer to check
 *
-* Return: Always 0.
+* Return: The absolute value of n
 */
-int main(void)
+int _abs(int n)
 {
-int r;
-r = _abs(-1);
-printf("%d\n", r);
-r = _abs(0);
-printf("%d\n", r);
-r = _abs(1);
-printf("%d\n", r);
-r = _abs(-98);
-printf("%d\n", r);
-return (0);
+if (n < 0)
+return -n;  // Return the negation if n is negative
+else
+return n;   // Return n if it is already positive or zero
 }
