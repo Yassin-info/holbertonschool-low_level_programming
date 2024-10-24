5135d5b76e71afad85e7e996af992f5d2423f372
root <yassin.jaghmim@gmail.com>
2024-10-24 16:02:40 +0000

    4-isalpha.c

--- functions_nested_loops/main_0.c
+++ functions_nested_loops/main_0.c
@@ -1,2 +1,17 @@
 #include "main.h"
-/* Rest of your main_0.c file */
+/**
+* test_isalpha - Tests the _isalpha function
+*/
+void test_isalpha(void)
+{
+int r;
+r = _isalpha('H');
+_putchar(r + '0');
+r = _isalpha('o');
+_putchar(r + '0');
+r = _isalpha(108); // ASCII for 'l'
+_putchar(r + '0');
+r = _isalpha(';'); // Not an alphabetic character
+_putchar(r + '0');
+_putchar('\n');
+}
