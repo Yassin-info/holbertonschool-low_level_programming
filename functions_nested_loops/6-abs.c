1a468690b676296659b52fef5c1fe0b2d594ee81
root <yassin.jaghmim@gmail.com>
2024-10-25 15:39:14 +0000

    6-abs.c

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * _abs - Computes the absolute value of an integer
-* @n: The integer to check
+* @n: The integer to compute the absolute value of
 *
 * Return: The absolute value of n
 */
