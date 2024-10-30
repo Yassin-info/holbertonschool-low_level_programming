f5329e3b2ea956579f4275777fe49518a3c8330e
root <yassin.jaghmim@gmail.com>
2024-10-30 23:26:41 +0000

    6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,14 +1,18 @@
 #include "main.h"
 /**
-* _puts - prints a string, followed by a new line, to stdout
-* @str: string to print
+* puts2 - prints every other character of a string
+* @str: the string to print
 */
-void _puts(char *str)
+void puts2(char *str)
 {
-while (*str != '\0')
+int i = 0;
+while (str[i] != '\0')
 {
-_putchar(*str);
-str++;
+if (i % 2 == 0)
+{
+_putchar(str[i]);
+}
+i++;
 }
 _putchar('\n');
 }
