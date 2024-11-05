621c7d4376eb7dcf6235af23d8196b0aa80f404e
root <yassin.jaghmim@gmail.com>
2024-11-05 12:06:49 +0000

    1-memcpy.c

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,13 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char *leet(char *);
-char *cap_string(char *);
-char *string_toupper(char *);
-void reverse_array(int *a, int n);
-int _strcmp(char *s1, char *s2);
-char *_strncpy(char *dest, char *src, int n);
-char *_strncat(char *dest, char *src, int n);
-char *_strcat(char *dest, char *src);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif /* MAIN_H */
