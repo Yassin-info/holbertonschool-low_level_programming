daaa3a7d7868fe34f931d4a672805fc6db458a44
root <yassin.jaghmim@gmail.com>
2024-11-09 00:30:15 +0000

    0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - prints the program name
+* @argc: argument count
+* @argv: argument vector
+*
+* Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+(void)argc; /* Unused parameter */
+printf("%s\n", argv[0]);
+return (0);
+}
