32ff1f528a7dcaf633817a4e9ef0245e3b1d6c41
root <yassin.jaghmim@gmail.com>
2024-11-15 11:46:30 +0000

    2-calloc.c

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+/**
+* _calloc - Allocates memory for an array and initializes it to zero
+* @nmemb: Number of elements in the array
+* @size: Size of each element in bytes
+*
+* Return: Pointer to the allocated memory, or NULL if it fails
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+void *ptr;
+unsigned int total_size;
+unsigned char *byte_ptr;
+unsigned int i;
+if (nmemb == 0 || size == 0)
+return (NULL);
+/* Check for multiplication overflow */
+if (nmemb > (unsigned int)-1 / size)
+return (NULL);
+total_size = nmemb * size;
+ptr = malloc(total_size);
+if (ptr == NULL)
+return (NULL);
+/* Initialize memory to zero */
+byte_ptr = (unsigned char *)ptr;
+for (i = 0; i < total_size; i++)
+byte_ptr[i] = 0;
+return (ptr);
+}
