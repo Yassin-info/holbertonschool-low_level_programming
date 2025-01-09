dc366ecd3c282db10fe1cff675debc1cc884e48c
yassin74 <yassin.jaghmim@gmail.com>
2025-01-09 12:33:45 +0000

    1-memcpy.c

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,12 +1,11 @@
 #include "main.h"
-#include <stdio.h>
 /**
-* _memcpy - Copies memory area
-* @dest: Pointer to the destination memory area
-* @src: Pointer to the source memory area
-* @n: Number of bytes to copy
+* _memcpy - Copie une zone mémoir
+* @dest: Pointeur vers la zone mémoire de destinatio
+* @src: Pointeur vers la zone mémoire sourc
+* @n: Nombre d'octets a copier
 *
-* Return: Pointer to dest
+* Return: Un pointeur vers la zone mémoire des
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
@@ -15,5 +14,5 @@ for (i = 0; i < n; i++)
 {
 dest[i] = src[i];
 }
-return dest;
+return (dest);
 }
