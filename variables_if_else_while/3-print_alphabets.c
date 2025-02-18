b717a5de0ddc50d119c5d2d793809d2bdfb235b9
root <yassin.jaghmim@gmail.com>
2025-02-18 09:38:25 +0000

    3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,12 +1,12 @@
-#include<stdio>
+#include<stdio.h>
 /**
 * main - Entry point
 *
 * Return: Always 0 (Success)
 */
 int main(void)
-int i;
 {
+int i;
 for (i = 0; i < 26; i++)
 {
 putchar(97 + i);
