085c2e94baefc621a3f80f1fb76e4b0dbad5fa84
root <yassin.jaghmim@gmail.com>
2025-02-24 09:05:12 +0000

    0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*_isupper - Checks for uppercase character
+*
+*@c: The character to be checked
+*
+*Return: 1 if c is uppercase, 0 otherwise
+*/
+int _isupper(void);
+{
+int c;
+{
+if (c >= 65 <= 90)
+return (1)
+{
+else (c >= 97 <= 122)
+return (0)
+}
+}
+}
