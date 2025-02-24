9758a777b8db16dfa044dd4d21be82aeea5f845a
root <yassin.jaghmim@gmail.com>
2025-02-24 14:44:50 +0000

    7-print_diagonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -9,14 +9,22 @@
 *and spaces. If n is 0 or less, it only prints a newline.
 */
 void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
 	{
-		int i;
-		if (n > 0)
-			{
-				for (i = 0; i < n; i++)
-					{
-						_putchar('\');
-						}
-						}
 		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
 		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
