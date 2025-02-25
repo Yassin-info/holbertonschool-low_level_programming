76c723477f9fcc880b3b69e39f8b75f84884c427
root <yassin.jaghmim@gmail.com>
2025-02-25 13:10:16 +0000

    9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -6,17 +6,26 @@
 *
 * Return: Always 0.
 */
-int main(void);
+int main(void)
 {
 	int i;
+
 	for (i = 1; i <= 100; i++)
-		{
-			if (3 % 100)
-			printf("%d Fizz")
-			else if (5 % 100)
-			printf("%d Buzz")
-			else if (3 % 100, 5 % 100)
-			printf("%d FizzBuzz")
-			}
-	return (0)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+
+		if (i < 100)
+			printf(" ");
+		else
+			printf("\n");
+	}
+
+	return (0);
 }
