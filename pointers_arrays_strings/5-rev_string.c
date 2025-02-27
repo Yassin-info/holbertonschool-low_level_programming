5c98133f6fc19a297c713b986768213ef389a4d9
yassin-info <yassin.jaghmim@gmail.com>
2025-02-27 09:02:00 +0000

    5-rev_string.c

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* rev_string - reverses a string
+* @s: pointer to the string to be reversed
+*/
+void rev_string(char *s)
+	{
+		int len = 0;
+		while (s[len] != '\0')
+			{
+				len 
