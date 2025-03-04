300bdab94c39a0edd0147b2ed562d72b16955e84
User <user@MacBook-Air-de-User.local>
2025-03-04 10:15:53 +0100

    0-memset.c

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
 * _memset - fills memory with a constant byte
 * @s: pointer to the memory area to be filled
@@ -9,11 +10,12 @@
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-     unsigned int i;
+unsigned int i;
 
-     for (i = 0; i < n; i++)
-     {
-        s[i] = b;
-        }
+for (i = 0; i < n; i++)
+{
+s[i] = b;
+}
+
 return (s);
 }
