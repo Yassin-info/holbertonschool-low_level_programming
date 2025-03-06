7feedd506d3cb92fecb6266113818fb0455720ae
User <user@air-de-user.home>
2025-03-06 12:08:10 +0100

    0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - Prints the name of the program
+* @argc: Arguments count
+* @argv: Array of args
+* Return: Always 0
+*/
+int main(int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", argv[0]);
+return (0);
+}
