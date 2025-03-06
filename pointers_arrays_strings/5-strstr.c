31a3e62aac43e315e5ef996a2c1f2fa9d833ea8f
User <user@MacBook-Air-de-User.local>
2025-03-06 10:27:01 +0100

    5-strstr.c

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* *_strstr - function that searches a string for any of a set of bytes.
+* @haystack: char *s = "hello, world"
+* @needle: char *f = "world";
+* Return: &haystack[i] si tte la sting est = à needle et 0 sinon
+*/
+char *_strstr(char *haystack, char *needle)
+{	
+int i;
+int j;
+for (i = 0; haystack[i] != '\0'; i++)
+{
+if (*needle == '\0')
+return (haystack);
+if (haystack[i] == needle[0])	
+{
+for (j = 0; needle[j] != '\0'; j++)
+{
+if (haystack[i + j] != needle[j])
+break;
+}
+if (needle[j] == '\0')
+return (&haystack[i]);
+}
+}
+return (0);
+}
