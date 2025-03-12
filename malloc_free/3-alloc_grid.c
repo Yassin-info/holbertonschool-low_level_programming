24dc7c7680d73c5863b22dff47d98b9a1b03dddf
User <user@MacBook-Air-de-User.local>
2025-03-12 09:58:02 +0100

    3-alloc_grid.c

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* alloc_grid - Creates a 2D integer grid initialized to 0
+* @width: Number of columns in the grid
+* @height: Number of rows in the grid
+* Return: Pointer to newly allocated 2D array, or NULL if:
+* - Width/height are 0/negative
+* - Memory allocation fails
+*/
+int **alloc_grid(int width, int height)
+{
+
+int **grid;
+int i, j;
+
+if (width <= 0 || height <= 0)
+return (NULL);
+
+grid = malloc(height * sizeof(int *));
+
+if (grid == NULL)
+return (NULL);
+
+for (i = 0; i < height; i++)
+{
+
+grid[i] = malloc(width * sizeof(int));
+
+if (grid[i] == NULL)
+{
+free_grid(grid, i);
+
+return (NULL);
+}
+
+for (j = 0; j < width; j++)
+{
+grid[i][j] = 0;
+}
+
+}
+return (grid);
+}
