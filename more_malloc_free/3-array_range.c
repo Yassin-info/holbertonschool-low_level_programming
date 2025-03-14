276398ca5313b8d453cd4976ddce3c8a5ba2c8f2
User <user@Air-de-User.lan>
2025-03-14 17:16:06 +0100

    2-calloc.c

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* *array_range - function that creates an array of integers.
+* @min: array[0]
+* @max: array[NULL - 1]
+* Return: array
+*/
+
+int *array_range(int min, int max)
+{
+
+int *y;
+int l;
+int i;
+
+if (min > max)
+return (NULL);
+
+
+y = malloc((max - min + 1) * sizeof(int));
+if (y == NULL)
+return (NULL);
+
+for (l = min, i = 0; l <= max; l++, i++)
+{
+y[i] = i;
+}
+
+return (y);
+}
