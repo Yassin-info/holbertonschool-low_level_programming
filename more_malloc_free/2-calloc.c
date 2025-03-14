49d449d89304784fc382443d226da010dd57b396
User <user@Air-de-User.lan>
2025-03-14 17:10:47 +0100

    2-calloc.c

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -14,13 +14,18 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 
 char *c;
-
+int l = 0;
+unsigned int i;
 if (nmemb == 0 || size == 0)
 return (NULL);
 
 c = malloc(nmemb * size);
 
 if (c == NULL)
-
-}
 return (NULL);
+for (i = 0; i < nmemb * size; i++)
+{
+c[i] = l;
+}
+return (c);
+}
