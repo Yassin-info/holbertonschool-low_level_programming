61249fa070283cc1344282e91900b8830bf227bf
User <user@Air-de-User.lan>
2025-03-14 16:59:57 +0100

    2-calloc.c

---
+++ malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * *_calloc - function that allocates memory for an array, using malloc.
+ * @nmemb: taille du tableau
+ * @size: taille octets
+ * Return: array
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+
+char *c;
+
+if (nmemb == 0 || size == 0)
+return (NULL);
+
+c = malloc(nmemb * size);
+
+if (c == NULL)
+return (NULL);
+
+}
