a4b0fa6eeee24a2f8ffd83e4fd8a212403550afb
User <user@MacBook-Air-de-User.local>
2025-03-17 16:29:07 +0100

    1-array_iterator.c

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -14,7 +14,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
     long unsigned int i;
     for (i = 0; i <= size; i++)
     {
-        printf("%d", array[i]);
+        printf("(%d)", array[i]);
         (*action)(array[i]);
         }
 }
