0a7f60cdc033f075c4df355d554c2a816a210441
User <user@MacBook-Air-de-User.local>
2025-03-17 14:54:49 +0100

    0-print_name.c

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -4,7 +4,7 @@
 /**
 * print_name_as_is - prints a name as is
 * @name: name of the person
-*
+* @f: Pointer to the printing function to use
 * Return: Nothing.
 */
 void print_name(char *name, void (*f)(char *))
