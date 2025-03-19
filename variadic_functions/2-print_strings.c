ad9034ac72840930a2910ed902e72a7837537b2b
User <user@MacBook-Air-de-User.local>
2025-03-19 14:09:07 +0100

    2-print_strings.c

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,6 +1,13 @@
 #include "variadic_functions.h"
 /** 
-
+* print_strings - Prints strings followed by a new line
+* @separator: String to be printed between the strings
+* @n: Number of strings passed to the function
+* @...: Variable number of strings to be printed
+* Description: This function prints a variable number of strings,
+* separated by the given separator string. If a string is NULL,
+* it prints (nil) instead. A new line is printed at the end.
+* Return: void
 */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
