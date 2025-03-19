6effee08fd86cff33b113a1a54b80794c8f676a4
User <user@MacBook-Air-de-User.local>
2025-03-19 12:28:59 +0100

    1-print_numbers.c

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,19 @@
+#include "variadic_functions.h"
+/**
+
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        print ("%d separator \n" i)
+    }
+    while (separator != NULL)
+    i++;
+
+    else
+    print (" ")
+
+    putchar ("\n")
+}
