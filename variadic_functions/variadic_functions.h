a3d6a9ac9d35d5910c3bc7b373398067408a0bf0
User <user@MacBook-Air-de-User.local>
2025-03-21 09:43:28 +0100

    3-print_all.c

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -5,10 +5,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stddef.h>
-#include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
