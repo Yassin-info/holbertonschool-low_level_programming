659886d52f993ebc8990b983f5fa43cd90530a60
User <user@MacBook-Air-de-User.local>
2025-03-21 09:29:51 +0100

    3-print_all.c

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,28 @@
+#include "variadic_function.h"
+/**
+
+*/
+void print_all(const char * const format, ...)
+{
+    va_list args;
+	unsigned int i = 0;
+	char *s;
+	int print = 0;
+
+	va_start(args, format);
+    switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(args, int));
+				print = 1;
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				print = 1;
+				break;
+                }
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
