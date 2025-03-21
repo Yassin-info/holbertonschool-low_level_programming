de0c53d0e9555b6978e6e7035b329ffd0b9de377
User <user@MacBook-Air-de-User.local>
2025-03-21 09:40:28 +0100

    3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,43 +1,50 @@
 #include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
 
 /**
-* print_all - Prints arguments of various types
-* @format: String with argument types
-*/
+ * print_all - imprime divers types arguments
+ * @format: chaine de caractere avec les types arguments
+ */
+
 void print_all(const char * const format, ...)
+
 {
-    va_list args;
+	va_list args;
 	unsigned int i = 0;
-	char *s;
+	char *str;
 	int printed = 0;
 
 	va_start(args, format);
-    switch (format[i])
+
+	while (format && format[i])
+	{
+		if (printed && (format[i] == 'c' || format[i] == 'i' ||
+						 format[i] == 'f' || format[i] == 's'))
+			printf(", ");
+
+		switch (format[i])
 		{
 			case 'c':
 				printf("%c", va_arg(args, int));
 				printed = 1;
 				break;
-			
-            case 'i':
+			case 'i':
 				printf("%d", va_arg(args, int));
 				printed = 1;
 				break;
-            
-            case 'f':
+			case 'f':
 				printf("%f", va_arg(args, double));
 				printed = 1;
 				break;
-			
-            case 's':
-				s = va_arg(args, char *);
-				printf("%s", s ? s : "(nil)");
+			case 's':
+				str = va_arg(args, char *);
+				printf("%s", str ? str : "(nil)");
 				printed = 1;
 				break;
-			
-                default:
+			default:
 				break;
-                }
+		}
 		i++;
 	}
 	printf("\n");
