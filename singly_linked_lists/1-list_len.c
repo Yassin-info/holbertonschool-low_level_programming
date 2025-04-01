e9f2259f0751f5cedb356c898f2a053cc3294cde
User <user@MacBook-Air-de-User.local>
2025-04-01 14:23:30 +0200

    1-list_len.c

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,15 +1,17 @@
 #include "lists.h"
 /**
-* 
+* list_len - returns the number of elements in a linked list_t list
+* @h: pointer to the head of the list
+* Return: number of elements in the list
 */
 size_t list_len(const list_t *h)
 {
-    size_t n = 0;
+size_t n = 0;
 
-    while (h != NULL)
-    {
-        n++;
-        h = h->next;
-	}
-	return (n);
+while (h != NULL)
+{
+n++;
+h = h->next;
+}
+return (n);
 }
