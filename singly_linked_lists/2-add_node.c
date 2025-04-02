ab543b869fbdd81627e686e3669e68ab76e937a8
User <user@air-de-user.home>
2025-04-02 10:50:56 +0200

    2-add_node.c

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -7,29 +7,29 @@
 */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new_node;
-    char *dup_str;
-    unsigned int len = 0;
+list_t *new_node;
+char *dup_str;
+unsigned int len = 0;
 
-    new_node = malloc(sizeof(list_t));
-    
-    if (new_node == NULL)
-        return (NULL);
-    
-    dup_str = strdup(str);
-    if (dup_str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
-    
-    while (str[len])
-        len++;
-    
-    new_node->str = dup_str;
-    new_node->len = len;
-    new_node->next = *head;
+new_node = malloc(sizeof(list_t));
 
-    *head = new_node;
-    return (new_node);
+if (new_node == NULL)
+return (NULL);
+
+dup_str = strdup(str);
+if (dup_str == NULL)
+{
+free(new_node);
+return (NULL);
+}
+
+while (str[len])
+len++;
+
+new_node->str = dup_str;
+new_node->len = len;
+new_node->next = *head;
+
+*head = new_node;
+return (new_node);
 }
