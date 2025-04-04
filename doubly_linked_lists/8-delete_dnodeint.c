5aacef5c7d789d33c12d849bad6c20870f296684
User <user@MacBook-Air-de-User.local>
2025-04-04 15:41:52 +0200

    8-delete_dnodeint.c

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -1,6 +1,9 @@
 #include "lists.h"
 /**
-* 
+* delete_dnodeint_at_index - deletes node at index of a dlistint_t linked list
+* @head: pointer to pointer to the head of the list
+* @index: index of the node to delete
+* Return: 1 if succeeded, -1 if failed
 */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
@@ -38,5 +41,5 @@ if (current->next != NULL)
 current->next->prev = current->prev;
 
 free(temp);
- return (1);
+return (1);
 }
