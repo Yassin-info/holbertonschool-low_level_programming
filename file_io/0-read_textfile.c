53be2788dba179cd6de4b6512ab3d334a255c7e5
User <user@Air-de-User.lan>
2025-04-04 22:17:42 +0200

    0-read_textfile.c

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -4,39 +4,46 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t nread, nwritten;
-    char *buffer;
+int fd;
+ssize_t nread, nwritten;
+char *buffer;
 
-    if (filename == NULL)
-        return (0);
+if (filename == NULL)
+return (0);
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+fd = open(filename, O_RDONLY);
 
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
+if (fd == -1)
+return (0);
+
+buffer = malloc(sizeof(char) * letters);
+
+if (buffer == NULL)
 {
-    close(fd);
-    return (0);
+close(fd);
+return (0);
 }
 nread = read(fd, buffer, letters);
-    if (nread == -1)
-	{
-        free(buffer);
-        close(fd);
-        return (0);
-    }
 
-    nwritten = write(STDOUT_FILENO, buffer, nread);
-    if (nwritten == -1 || nwritten != nread)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
-    free(buffer);
-    close(fd);
-    return (nwritten);
+if (nread == -1)
+{
+free(buffer);
+close(fd);
+
+return (0);
+}
+
+nwritten = write(STDOUT_FILENO, buffer, nread);
+
+if (nwritten == -1 || nwritten != nread)
+{
+free(buffer);
+close(fd);
+
+return (0);
+}
+free(buffer);
+close(fd);
+
+return (nwritten);
 }
