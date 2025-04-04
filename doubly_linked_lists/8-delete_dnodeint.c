a17908f224bbd226909bbb5d1aaed24cfddbb814
User <user@MacBook-Air-de-User.local>
2025-04-04 15:40:01 +0200

    8-delete_dnodeint.c

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -4,34 +4,39 @@
 */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *current, *temp;
-    unsigned int i;
+dlistint_t *current, *temp;
+unsigned int i;
 
-    if (head == NULL || *head == NULL)
-        return (-1);
+if (head == NULL || *head == NULL)
+return (-1);
 
-    current = *head;
+current = *head;
 
-    if (index == 0)
-    {
-        *head = current->next;
-        if (*head != NULL)
-            (*head)->prev = NULL;
-        free(current);
-        return (1);
-    }
-    for (i = 0; i < index && current != NULL; i++)
-        current = current->next;
+if (index == 0)
+{
+*head = current->next;
 
-    if (current == NULL)
-        return (-1);
+if (*head != NULL)
+(*head)->prev = NULL;
+free(current);
 
-    temp = current;
-    if (current->prev != NULL)
-        current->prev->next = current->next;
-    if (current->next != NULL)
-        current->next->prev = current->prev;
+return (1);
+}
 
-    free(temp);
-    return (1);
+for (i = 0; i < index && current != NULL; i++)
+current = current->next;
+
+if (current == NULL)
+return (-1);
+
+temp = current;
+
+if (current->prev != NULL)
+current->prev->next = current->next;
+
+if (current->next != NULL)
+current->next->prev = current->prev;
+
+free(temp);
+ return (1);
 }
