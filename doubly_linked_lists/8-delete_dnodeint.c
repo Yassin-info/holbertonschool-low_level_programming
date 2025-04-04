be3f46e5e377171fcde72f2139d409fe6bcde79a
User <user@MacBook-Air-de-User.local>
2025-04-04 15:34:37 +0200

    8-delete_dnodeint.c

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+* 
+*/
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+    dlistint_t *current, *temp;
+    unsigned int i;
+
+    if (head == NULL || *head == NULL)
+        return (-1);
+
+    current = *head;
+
+    if (index == 0)
+    {
+        *head = current->next;
+        if (*head != NULL)
+            (*head)->prev = NULL;
+        free(current);
+        return (1);
+    }
+}
